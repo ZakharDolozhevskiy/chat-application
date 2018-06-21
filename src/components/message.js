37aa16427b50575d2f90d041d27b3eadf8f2091d
zdolozhevskiy <zakhar.dolozhevskyi@pipedrive.com>
2018-06-21 19:39:22 +0300

    implement hot keys and anchor links support

--- src/components/message.js
+++ src/components/message.js
@@ -1,25 +1,26 @@
 import React from "react";
 import styled from 'styled-components';
-import ReactPlayer from 'react-player'
+import Linkify from 'react-linkify';
+import ReactPlayer from 'react-player';
 
 export class Message extends React.PureComponent {
-	renderImages = links =>
-		links.map((link, idx) => <img src={link} key={idx}/>);
+  renderImages = links =>
+    links.map((link, idx) => <img src={link} key={idx}/>);
 
-	renderVideo = links =>
-		links.map((link, idx) => <ReactPlayer url={link} key={idx}/>);
+  renderVideo = links =>
+    links.map((link, idx) => <ReactPlayer url={link} key={idx}/>);
 
-	render() {
-		const { message, videoLinks, imageLinks, className } = this.props;
+  render() {
+    const {message, videoLinks, imageLinks, className} = this.props;
 
-		return (
-			<div className={className}>
-				{imageLinks && this.renderImages(imageLinks)}
-				{videoLinks && this.renderVideo(videoLinks)}
-				<em>{message}</em>
-			</div>
-		);
-	}
+    return (
+      <div className={className}>
+        {imageLinks && this.renderImages(imageLinks)}
+        {videoLinks && this.renderVideo(videoLinks)}
+        <Linkify>{message}</Linkify>
+      </div>
+    );
+  }
 }
 
 export default styled(Message)`
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,55 +1,83 @@
-import React from "react";
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { connect } from 'react-redux';
+import throttle from 'lodash.throttle';
+import {connect} from 'react-redux';
 
 import Icon from "@material-ui/core/es/Icon/Icon";
 import Button from "@material-ui/core/es/Button/Button";
 import Message from '../components/message';
 import TextField from "@material-ui/core/es/TextField/TextField";
 
-import { sendMessage } from '../actions/messages';
+import {sendMessage} from '../actions/messages';
 
 export class Chat extends React.Component {
-	state = { message: '' };
+  state = { message: '' };
 
-	onInputChange = (event) =>
-		this.setState({ message: event.target.value });
+  onInputChange = (event) =>
+    this.setState({ message: event.target.value });
 
-	onSendBtnClick = () =>
-		this.props.sendMessage({
-			message: this.state.message,
-			username: this.props.username
-		});
+  trackHotKeys = throttle((event) => {
+    if (this.props.hotKeys && this.isSendMsgComb(event)) {
+      this.sendMessage();
+    }
+  }, 100);
 
-	render() {
-		return (
-			<div>
-				<Paper elevation={4}>
-						<div>
-							{this.props.messages.map(
-								(m, i) => <Message key={i} message={m.message} imageLinks={m.imageLinks} videoLinks={m.videoLinks}/>
-							)}
-						</div>
-					<TextField
-						id="name"
-						label="Name"
-						value={this.state.message}
-						onChange={this.onInputChange}
-						margin="normal"
-					/>
-					<Button variant="fab" color="secondary" aria-label="edit" onClick={this.onSendBtnClick}>
-						<Icon>edit_icon</Icon>
-					</Button>
-				</Paper>
-			</div>
-		)
-	}
+  isSendMsgComb = event =>
+    (event.ctrlKey || event.metaKey) &&
+    (event.keyCode === 13 || event.keyCode === 10);
+
+  sendMessage = () => {
+    if (!this.state.message) {
+      return;
+    }
+
+    this.props.sendMessage({
+      message: this.state.message,
+      username: this.props.username
+    });
+
+    this.setState({ message: '' });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.trackHotKeys);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.trackHotKeys);
+  }
+
+  render() {
+    return (
+      <div>
+        <Paper elevation={4}>
+          <div>
+            {this.props.messages.map(
+              (m, i) => <Message key={i} message={m.message} imageLinks={m.imageLinks} videoLinks={m.videoLinks}/>
+            )}
+          </div>
+          <TextField
+            id="name"
+            label="Name"
+            autoFocus
+            value={this.state.message}
+            onChange={this.onInputChange}
+            margin="normal"
+          />
+          <Button variant="fab" color="secondary" aria-label="edit" onClick={this.sendMessage}>
+            <Icon>edit_icon</Icon>
+          </Button>
+        </Paper>
+      </div>
+    )
+  }
 }
 
 const stateToProps = (store) => ({
-	messages: store.messages,
-	username: store.settings.username,
-	timeFormat: store.settings.timeFormat
+  messages: store.messages,
+  hotKeys: store.settings.hotKeys,
+  username: store.settings.username,
+  timeFormat: store.settings.timeFormat
 });
 
-export default connect(stateToProps, { sendMessage })(Chat);
+export default connect(stateToProps, {sendMessage})(Chat);
